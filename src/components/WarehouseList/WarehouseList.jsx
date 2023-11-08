2ae6bb5f729543ba33d121f11df679e6f5efab98
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-08 08:46:49 -0800

    add classnames and style plus flexbox to button and main details. add dummy data

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,12 +1,34 @@
+import "../WarehouseList/warehouseList.scss";
+
 function WarehouseList() {
   return (
     <>
       <div className="warehouseListContainer">
-        <div className="warehouseListContainer__title">
-          <h1>Warehouses</h1>
-          <input></input>
-          <button></button>
-          <div></div>
+        <div className="warehouseListContainer__titleBox">
+          <h1 className="warehouseListContainer__title">Warehouses</h1>
+          <input
+            className="warehouseListContainer__searchBar"
+            placeholder="Search..."
+          ></input>
+          <button className="warehouseListContainer__searchButton">
+            +Add New Warehouse
+          </button>
+        </div>
+        <div className="warehouseListContainer__detailsBox">
+          <div className="warehouseListContainer__detailsBox--left">
+            <p>WAREHOUSE</p>
+            <p>Manhattan</p>
+            <p>ADDRESS</p>
+            <p>503 Broadway, New York, USA</p>
+            <img></img>
+          </div>
+          <div className="warehouseListContainer__detailsBox--right">
+            <p>CONTACT NAME</p>
+            <p>CONTACT INFORMATION</p>
+            <p>+1(629)555-0129</p>
+            <p>paujla@instock.com</p>
+            <img></img>
+          </div>
         </div>
       </div>
     </>
--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,11 +1,16 @@
 import Footer from "../../components/Footer/Footer";
 
+// delete this before pull
+import WarehouseList from "../../components/WarehouseList/WarehouseList";
+
 function Warehouses() {
-    return (
-        <>
-        <h1>Hello world</h1>
-        <Footer />
-        </>
-    );
+  return (
+    <>
+      <h1>Hello world</h1>
+      {/* delete this before pull */}
+      <WarehouseList />
+      <Footer />
+    </>
+  );
 }
 export default Warehouses;
