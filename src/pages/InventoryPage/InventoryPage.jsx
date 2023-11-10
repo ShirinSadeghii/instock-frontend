2424e0bafbae2ac62f61566a3d4f9cbdff86b5a1
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-10 14:47:40 -0800

    creating delete modal for inventory

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,17 +1,20 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "../../App.scss";
-import Inventory from "../../components/Inventory/Inventory"
+import Inventory from "../../components/Inventory/Inventory";
+import modal from "./modal";
+import Modal from "../../components/Warehouse/modal";
 
 function InventoryPage() {
   return (
     <div className="warehouse-details">
-    <Header />
-    <div className="main">
-      <Inventory />
-      <Footer />
+      <Header />
+      <div className="main">
+        <Inventory />
+        <Footer />
+        <Modal />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
-export default InventoryPage
+export default InventoryPage;
