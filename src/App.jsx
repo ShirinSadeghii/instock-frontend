f994cd15991d04275eb74ef0a52ae39688255801
pdvyas26 <pdvyas26@gmail.com>
2023-11-10 09:50:42 -0800

    complted inventroy list page fuly responsive, complete components add inventory, edit inventory fully responsive add make footer fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Inventory from "./pages/Inventory/Inventory";
 import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails";
 import Warehouses from "./pages/Warehouses/Warehouses";
 import "./App.scss";
+import AddInventory from "./components/AddInventory/AddInventory";
+import EditInventory from "./components/EditInventory/EditInventory";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<Warehouses />}></Route>
           <Route path="/details" element={<WarehouseDetails />}></Route>
           <Route path="/inventory" element={<Inventory />}></Route>
+          <Route path="/add-inventory" element={<AddInventory />}></Route>
+          <Route path="/edit-inventory" element={<EditInventory />}></Route>
         </Routes>
       </BrowserRouter>
    
