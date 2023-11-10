9f43c9bc985cfc93d4d467150c660a05067a3410
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-10 15:25:35 -0800

    started delete warehouse page

--- src/components/WarehouseList/modal.jsx
+++ src/components/WarehouseList/modal.jsx
@@ -5,23 +5,26 @@ const Modal = ({ showModal, closeModal, handleDelete, deleteWarehouse }) => {
   return (
     <div className={`modal ${showModal ? "show" : ""}`}>
       <div className="modal-content">
-        <span className="close" onClick={closeModal}>
-          cancel
-        </span>
-        <button
-          onClick={() => {
-            handleDelete();
-            deleteWarehouse?.id();
-          }}
-        >
-          Delete Warehouse
-        </button>
         <h2> Delete {deleteWarehouse?.warehouse_name} warehouse?</h2>
         <p>
           Please confirm that you'd like to delete the{" "}
           {deleteWarehouse?.warehouse_name} from the list of warehouses. You
           won't be able to undo this action.{" "}
         </p>
+        <div className="selectionBox">
+          <button className="selectionBox__cancel" onClick={closeModal}>
+            cancel
+          </button>
+          <button
+            className="selectionBox__delete"
+            onClick={() => {
+              handleDelete();
+              deleteWarehouse?.id();
+            }}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
