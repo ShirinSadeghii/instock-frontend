436d91eb8cf7ff4c92374d9652650e172f746456
pdvyas26 <pdvyas26@gmail.com>
2023-11-10 17:07:18 -0800

    add drop down functionality for category and warehouse for add and edit inventroy component

--- src/components/AddInventory/AddInventory.jsx
+++ src/components/AddInventory/AddInventory.jsx
@@ -2,9 +2,37 @@ import backArrow from '../../assets/Icons/arrow_back-24px.svg';
 import './AddInventory.scss';
 import dropDownArrow from '../../assets/Icons/arrow_drop_down-24px.svg';
 import '../Warehouse/Warehouse.scss';
+import { useState } from 'react';
 
 
 function AddInventory() {
+
+    const [stockStatus, setStockStatus] = useState('InStock');
+    const [selectedWarehouse, setSelectedWarehouse] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+
+
+    // Event handler for when the radio button selection changes
+    const handleStockStatusChange = (event) => {
+      setStockStatus(event.target.value);
+    };
+
+    // Event handler for when the warehouse selection changes
+    const handleWarehouseChange = (event) => {
+        setSelectedWarehouse(event.target.value);
+    };
+
+      // Event handler for when the category selection changes
+      const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    // Dummy warehouse names
+    const warehouses = ["Warehouse 1", "Warehouse 2", "Warehouse 3", "Warehouse 4", "Warehouse 5", "Warehouse 6", "Warehouse 7", "Warehouse 8"];
+    const categories = ["Category 1", "Category 2", "Category 3", "Category 4", "Category 5"];
+
+
   return (
     <div className="warehouse">
         <div className="newInv-heading">
@@ -19,32 +47,56 @@ function AddInventory() {
                 <p className="newInv__item-name">Description</p>
                 <textarea className="newInv__item-descriptionInput" placeholder="Please enter a brief item description" id="" cols="30" rows="10"></textarea>
                 <p className="newInv__item-name">Category</p>
-                <button className="newInv__btn newInv__item-input" name="" placeholder="Please Select" id="" cols="30" rows="1">
-                    <p>Please select</p>
-                    <img src={dropDownArrow} alt="" />
-                </button>
-                
+                <select 
+                    className="newInv__item-input" 
+                    value={selectedCategory} 
+                    onChange={handleCategoryChange}
+                >
+                    <option value="">Please Select</option>
+                    {categories.map((category, index) => (
+                        <option key={index} value={category}>{category}</option>
+                    ))}
+                </select>
             </div>
             <div className="newInv-itemAvailability">
                 <p className="newInv__item-heading">Item Availability</p>
                 <p className="newInv__item-name">Status</p>
-                <div className="newInv__radio" >
-                    <label>
-                        <input type="radio" />
+                <div className="newInv__radio">
+                    <label className="newInv__radio-container">
+                        <input 
+                        type="radio" 
+                        name="stockStatus" 
+                        value="InStock" 
+                        checked={stockStatus === 'InStock'} 
+                        onChange={handleStockStatusChange} 
+                        />
                         <span className="newInv__radio-label">In stock</span>
                     </label>
-                    <label >
-                        <input placeholder="Out Of Stock" type="radio" />
+                    <label className="newInv__radio-container">
+                        <input 
+                        type="radio" 
+                        name="stockStatus" 
+                        value="OutOfStock" 
+                        checked={stockStatus === 'OutOfStock'} 
+                        onChange={handleStockStatusChange} 
+                        />
                         <span className="newInv__radio-label">Out of stock</span>
                     </label>
                 </div>
+
                 <p className="newInv__item-name">Quantity</p>
                 <input className="newInv__item-input" type="text" placeholder="0" />
                 <p className="newInv__item-name">Warehouse</p>
-                <button className="newInv__btn newInv__item-input" placeholder="Please Select" id="" cols="30" rows="1">
-                    <p>Please select</p>
-                    <img src={dropDownArrow} alt="" />
-                </button>
+                <select 
+                    className="newInv__item-input" 
+                    value={selectedWarehouse} 
+                    onChange={handleWarehouseChange}
+                >
+                    <option value="">Please Select</option>
+                    {warehouses.map((warehouse, index) => (
+                        <option key={index} value={warehouse}>{warehouse}</option>
+                    ))}
+                </select>
             </div>
         </div>
         <div className="newInv__btn-container">
@@ -54,4 +106,4 @@ function AddInventory() {
         </div>
   )
 }
-export default AddInventory
+export default AddInventory;
