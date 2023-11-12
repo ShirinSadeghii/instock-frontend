d436a7265566cc3f87c5a8b2d9be896a2a245d54
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-11-11 18:31:13 -0800

    update inventory page modal with styling from warehouse page for mobile desktop and tablet

--- src/components/Inventory/modal.jsx
+++ src/components/Inventory/modal.jsx
@@ -6,23 +6,25 @@ const Modal = ({ showModal, closeModal, handleDelete, deleteWarehouse }) => {
   return (
     <div className={`modal ${showModal ? "show" : ""}`}>
       <div className="modal-content">
-        <div className="deleteTitleLogoContainer">
-          <h2 className="deleteTitleLogoContainer__title">
-            {" "}
-            Delete {deleteWarehouse?.warehouse_name} inventory item?
-          </h2>{" "}
-          <img
-            className="deleteTitleLogoContainer__logo"
-            src={xLogo}
-            alt="close window logo"
-          />
+        <div className="deleteTitleLogoContainer__outerBox">
+          <div className="deleteTitleLogoContainer">
+            <h2 className="deleteTitleLogoContainer__title">
+              {" "}
+              Delete {deleteWarehouse?.warehouse_name} inventory item?
+            </h2>{" "}
+            <img
+              className="deleteTitleLogoContainer__logo"
+              src={xLogo}
+              alt="close window logo"
+            />
+          </div>
+          <div className="warehouseListModaldetails_container"></div>
+          <p>
+            Please confirm that you'd like to delete the{" "}
+            {deleteWarehouse?.warehouse_name} from the list of warehouses. You
+            won't be able to undo this action.{" "}
+          </p>
         </div>
-        <div className="warehouseListModaldetails_container"></div>
-        <p>
-          Please confirm that you'd like to delete the{" "}
-          {deleteWarehouse?.warehouse_name} from the list of warehouses. You
-          won't be able to undo this action.{" "}
-        </p>
         <div className="selectionBox">
           <button className="selectionBox__cancel" onClick={closeModal}>
             Cancel
