d7eec815a9663748ea48151fdd40be87b6e520c8
DaaniyaalM <daaniyaalmirza@gmail.com>
2023-11-11 20:39:21 -0800

    added post and edit axios requests for add warehouse and edit warehouse pages and applied them with an onClick to <img> classes with the title Add Warehouse

--- src/components/AddInventory/AddInventory.jsx
+++ src/components/AddInventory/AddInventory.jsx
@@ -1,57 +1,79 @@
-import backArrow from '../../assets/Icons/arrow_back-24px.svg';
-import './AddInventory.scss';
-import dropDownArrow from '../../assets/Icons/arrow_drop_down-24px.svg';
-import '../Warehouse/Warehouse.scss';
-
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import "./AddInventory.scss";
+import dropDownArrow from "../../assets/Icons/arrow_drop_down-24px.svg";
+import "../Warehouse/Warehouse.scss";
 
 function AddInventory() {
+  
   return (
     <div className="warehouse">
-        <div className="newInv-heading">
-            <img className="newInv-heading__img" src={backArrow} alt="Back Arrow" />
-            <span>Add New Inventory Item</span>
+      <div className="newInv-heading">
+        <img className="newInv-heading__img" src={backArrow} alt="Back Arrow" />
+        <span>Add New Inventory Item</span>
+      </div>
+      <div className="newInv-container">
+        <div className="newInv-itemDetails">
+          <p className="newInv__item-heading">Item Details</p>
+          <p className="newInv__item-name">Item Name</p>
+          <input
+            className="newInv__item-input"
+            type="text"
+            placeholder="Item Name"
+          />
+          <p className="newInv__item-name">Description</p>
+          <textarea
+            className="newInv__item-descriptionInput"
+            placeholder="Please enter a brief item description"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <p className="newInv__item-name">Category</p>
+          <button
+            className="newInv__btn newInv__item-input"
+            name=""
+            placeholder="Please Select"
+            id=""
+            cols="30"
+            rows="1"
+          >
+            <p>Please select</p>
+            <img src={dropDownArrow} alt="" />
+          </button>
         </div>
-        <div className="newInv-container">
-            <div className="newInv-itemDetails">
-                <p className="newInv__item-heading">Item Details</p>
-                <p className="newInv__item-name">Item Name</p>
-                <input className="newInv__item-input" type="text" placeholder="Item Name" />
-                <p className="newInv__item-name">Description</p>
-                <textarea className="newInv__item-descriptionInput" placeholder="Please enter a brief item description" id="" cols="30" rows="10"></textarea>
-                <p className="newInv__item-name">Category</p>
-                <button className="newInv__btn newInv__item-input" name="" placeholder="Please Select" id="" cols="30" rows="1">
-                    <p>Please select</p>
-                    <img src={dropDownArrow} alt="" />
-                </button>
-                
-            </div>
-            <div className="newInv-itemAvailability">
-                <p className="newInv__item-heading">Item Availability</p>
-                <p className="newInv__item-name">Status</p>
-                <div className="newInv__radio" >
-                    <label>
-                        <input type="radio" />
-                        <span className="newInv__radio-label">In stock</span>
-                    </label>
-                    <label >
-                        <input placeholder="Out Of Stock" type="radio" />
-                        <span className="newInv__radio-label">Out of stock</span>
-                    </label>
-                </div>
-                <p className="newInv__item-name">Quantity</p>
-                <input className="newInv__item-input" type="text" placeholder="0" />
-                <p className="newInv__item-name">Warehouse</p>
-                <button className="newInv__btn newInv__item-input" placeholder="Please Select" id="" cols="30" rows="1">
-                    <p>Please select</p>
-                    <img src={dropDownArrow} alt="" />
-                </button>
-            </div>
+        <div className="newInv-itemAvailability">
+          <p className="newInv__item-heading">Item Availability</p>
+          <p className="newInv__item-name">Status</p>
+          <div className="newInv__radio">
+            <label>
+              <input type="radio" />
+              <span className="newInv__radio-label">In stock</span>
+            </label>
+            <label>
+              <input placeholder="Out Of Stock" type="radio" />
+              <span className="newInv__radio-label">Out of stock</span>
+            </label>
+          </div>
+          <p className="newInv__item-name">Quantity</p>
+          <input className="newInv__item-input" type="text" placeholder="0" />
+          <p className="newInv__item-name">Warehouse</p>
+          <button
+            className="newInv__btn newInv__item-input"
+            placeholder="Please Select"
+            id=""
+            cols="30"
+            rows="1"
+          >
+            <p>Please select</p>
+            <img src={dropDownArrow} alt="" />
+          </button>
         </div>
-        <div className="newInv__btn-container">
-            <button className="newInv__cancel-btn">Cancel</button>
-            <button className="newInv__btn-blue"> + Add Item</button>
-        </div>
-        </div>
-  )
+      </div>
+      <div className="newInv__btn-container">
+        <button className="newInv__cancel-btn">Cancel</button>
+        <button className="newInv__btn-blue"> + Add Item</button>
+      </div>
+    </div>
+  );
 }
-export default AddInventory
+export default AddInventory;
--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -1,66 +1,136 @@
 import ArrowBack from "../../assets/Icons/arrow_back-24px.svg";
 import "../Warehouse/Warehouse.scss";
 import "../NewWarehouse/NewWarehouse.scss";
-import {useNavigate} from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 function EditWarehouse() {
+  const putWarehouse = async (warehouseId) => {
+    try {
+      const response = await axios.put(
+        `http://3.20.237.64:80/warehouses/${warehouseId}`
+      );
+      console.log(" put successful:", response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-const navigate = useNavigate();
- 
-function handleClick (event) {
-  const cancelSubmit = () => {
+  const navigate = useNavigate();
+
+  function handleClick(event) {
+    const cancelSubmit = () => {
       navigate("/");
+    };
+    cancelSubmit();
   }
-  cancelSubmit();
-}
-return (
-  <section className="warehouse">
-      <div className='warehouse__header'>
-          <div className='warehouse__container'>
-              <img onClick={(handleClick) => navigate('/')} src={ArrowBack} alt="arrow back icon"></img>
-              <h1 className='warehouse__title'>Edit Warehouse</h1>
-          </div>
+  return (
+    <section className="warehouse">
+      <div className="warehouse__header">
+        <div className="warehouse__container">
+          <img
+            onClick={(handleClick) => navigate("/")}
+            src={ArrowBack}
+            alt="arrow back icon"
+          ></img>
+          <h1 className="warehouse__title">Edit Warehouse</h1>
+        </div>
       </div>
-      <div className='warehouse-parent'>
-          <div className='warehouse-detail'>
-              <h2 className='warehouse-detail__title'>Warehouse Details</h2>
-              <form className='warehouse-detail__form'> 
-                  <label className='warehouse-detail__label'>Warehouse Name</label>
-                  <input className='warehouse-detail__input' type="text" name="WarehouseName" placeholder='Warehouse Name' required></input>
-                  <label className='warehouse-detail__label'>Street Address</label>
-                  <input className='warehouse-detail__input' type="text" name="StreetAddress" placeholder='Street Address' required></input>
-                  <label className='warehouse-detail__label'>City</label>
-                  <input className='warehouse-detail__input' type="text" name="City" placeholder='City' required></input>
-                  <label className='warehouse-detail__label'>Country</label>
-                  <input className='warehouse-detail__input' type="text" name="Country" placeholder='Country' required></input>
-              </form>
-          </div>
-          <div className='warehouse-detail__divider'></div>
-          <div className='warehouse-detail'>
-              <h2 className='warehouse-detail__title'>Contact Details</h2>
-              <form  className='warehouse-detail__form'> 
-                  <label className='warehouse-detail__label'>Contact Name</label>
-                  <input className='warehouse-detail__input' type="text" name="ContactName" placeholder='Contact Name' required></input>
-                  <label className='warehouse-detail__label'>Position</label>
-                  <input className='warehouse-detail__input' type="text" name="Position" placeholder='Position' required></input>
-                  <label className='warehouse-detail__label'>Phone Number</label>
-                  <input className='warehouse-detail__input' type="text" name="PhoneNumber" placeholder='Phone Number' required></input>
-                  <label className='warehouse-detail__label'>Email</label>
-                  <input className='warehouse-detail__input' type="text" name="Email" placeholder='Email' required></input>
-                  <div className='buttons--blue__container'>
-                        <button className='buttons buttons--blue'>+Add Warehouse</button>
-                  </div>
-              </form>
-          </div>
+      <div className="warehouse-parent">
+        <div className="warehouse-detail">
+          <h2 className="warehouse-detail__title">Warehouse Details</h2>
+          <form className="warehouse-detail__form">
+            <label className="warehouse-detail__label">Warehouse Name</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="WarehouseName"
+              placeholder="Warehouse Name"
+              required
+            ></input>
+            <label className="warehouse-detail__label">Street Address</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="StreetAddress"
+              placeholder="Street Address"
+              required
+            ></input>
+            <label className="warehouse-detail__label">City</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="City"
+              placeholder="City"
+              required
+            ></input>
+            <label className="warehouse-detail__label">Country</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="Country"
+              placeholder="Country"
+              required
+            ></input>
+          </form>
+        </div>
+        <div className="warehouse-detail__divider"></div>
+        <div className="warehouse-detail">
+          <h2 className="warehouse-detail__title">Contact Details</h2>
+          <form className="warehouse-detail__form">
+            <label className="warehouse-detail__label">Contact Name</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="ContactName"
+              placeholder="Contact Name"
+              required
+            ></input>
+            <label className="warehouse-detail__label">Position</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="Position"
+              placeholder="Position"
+              required
+            ></input>
+            <label className="warehouse-detail__label">Phone Number</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="PhoneNumber"
+              placeholder="Phone Number"
+              required
+            ></input>
+            <label className="warehouse-detail__label">Email</label>
+            <input
+              className="warehouse-detail__input"
+              type="text"
+              name="Email"
+              placeholder="Email"
+              required
+            ></input>
+            <div className="buttons--blue__container">
+              <button
+                className="buttons buttons--blue"
+                onClick={() => {
+                  putWarehouse();
+                }}
+              >
+                +Add Warehouse
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      
-      <div className='warehouse-detail__btn'>
-          <div className='warehouse-detail__btn-container'>
-              <button onClick={handleClick} className='buttons'>Cancel</button>
-              
-          </div>
+
+      <div className="warehouse-detail__btn">
+        <div className="warehouse-detail__btn-container">
+          <button onClick={handleClick} className="buttons">
+            Cancel
+          </button>
+        </div>
       </div>
-  </section>
+    </section>
   );
 }
 
