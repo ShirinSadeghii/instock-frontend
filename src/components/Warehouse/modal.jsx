189f3531d548ea48f32a633e1e80cbfa8f53a1ce
Shirin <shirinsadeghi@live.com>
2023-11-13 15:08:29 -0800

    adjust minor styling in WarehouseList.scss for Warehouses Page. Also edit WarehouseDetails modal to reflect same styling as Inventory modal

--- src/components/Warehouse/modal.jsx
+++ src/components/Warehouse/modal.jsx
@@ -1,26 +1,44 @@
 import React, { useState } from "react";
 import "../WarehouseList/modal.scss";
+import xLogo from "../../assets/Icons/close-24px.svg";
 
 const Modal = ({ showModal, closeModal, handleDelete, deletedItem }) => {
   return (
     <div className={`modal ${showModal ? "show" : ""}`}>
       <div className="modal-content">
-        <span className="close" onClick={closeModal}>
-          cancel
-        </span>
-        <button
-          onClick={() => {
-            handleDelete(deletedItem?.id);
-          }}
-        >
-          Delete Warehouse
-        </button>
-        <h2> Delete {deletedItem?.warehouse_name} warehouse?</h2>
-        <p>
-          Please confirm that you'd like to delete the{" "}
-          {deletedItem?.warehouse_name} from the list of warehouses. You
-          won't be able to undo this action.{" "}
-        </p>
+        <div className="deleteTitleLogoContainer__outerBox">
+          <div className="deleteTitleLogoContainer">
+            <h2 className="deleteTitleLogoContainer__title">
+              Delete {deletedItem?.item_name} inventory item?
+            </h2>{" "}
+            <img
+              onClick={closeModal}
+              className="deleteTitleLogoContainer__logo"
+              src={xLogo}
+              alt="close window logo"
+            />
+          </div>
+          <div className="warehouseListModaldetails_container">
+            <p>
+              Please confirm that you'd like to delete the{" "}
+              {deletedItem?.item_name} from the list of warehouses. You
+              won't be able to undo this action.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="selectionBox">
+          <button className="selectionBox__cancel" onClick={closeModal}>
+            Cancel
+          </button>
+          <button
+            className="selectionBox__delete"
+            onClick={() => {
+              handleDelete(deletedItem.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
